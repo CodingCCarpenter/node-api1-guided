56ccc301511ad51589553dcf27d7f25863810322
CodingCCarpenter <christineassists@aol.com>
2020-01-14 22:18:41 -0600

    creating get requests and setting global middleware

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 const express = require('express');
+const db = require('./data/hubs-model.js');
 
 const server = express();
 
 server.listen(4000, () => {
     console.log('*** listening on port 4000 ***');
-})
+});
+
+//global middleware section
+server.use(express.json());
+
+/* 
+GET https://my.host.example.com/my/path/to/some/resource?paremeters=list
+DELETE https://my.host.example.com/my/path/to/some/resource?paremeters=list
+PUT https://my.host.example.com/my/path/to/some/resource?paremeters=list
+POST https://my.host.example.com/my/path/to/some/resource?paremeters=list
+*/
+
+server.get('/', (req, res) => {
+    res.send('hello world!');
+});
+
+server.get('/now', (req, res) => {
+    res.send(new Date().toISOString());
+});
+
+//CRUD
+// C - create - POST  
+// R - read - GET
+// U - update - PUT
+// D - delete (destroy) - DELETE
+
+// ************************************************
+// retrieve info from the db
+// ************************************************
+server.get('/hubs', (req, res) => {
+    db.find()
+        .then(hubs => {
+            res.status(200).json(hubs);
+        })
+        .catch(err => {
+            res.status(500).json({success:false, err});
+        });
+});
+
+// ************************************************
+// add a record to the db
+// ************************************************
+
+server.post('/hubs', (req, res) => {
+    const hubInfo = req.body; 
+
+    db.add(hubInfo)
+        .then(hubs => {
+           res.status(201).json({success:true, hub}); 
+        })
+        .catch(err => {
+            res.status(500).json({success:false, err});
+        });
+});
